42f26f0ffa59da0d2860b368be7fe73957afe3f8
KockaAdmiralac <luka.kocka@gmail.com>
2015-10-17 23:06:07 +0200

    Initial commit

---
+++ html/src/rs/kockasystems/auraluxtest/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package rs.kockasystems.auraluxtest.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import rs.kockasystems.auraluxtest.AuraluxTest;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new AuraluxTest();
+        }
+}
